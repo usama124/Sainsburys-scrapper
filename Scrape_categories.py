e127e464114a551dd18890a4eb38d0cbe6f5ebb4
Usama Tahir <utahir.itp@sparkcognition.com>
2021-11-26 19:23:53 +0500

    Updated code for add penny price processing added ml weight processing added code to click no thanks when asked by website

--- Scrape_categories.py
+++ Scrape_categories.py
@@ -1,3 +1,4 @@
+import json
 import re, time
 from bs4 import BeautifulSoup
 import DownloadImage as downloader
@@ -11,22 +12,28 @@ def write_scraped_products(url):
 
 
 def increase_price_15_percent(price):
-    price = price.replace("£", "").strip()
-    price = float(price)
-    price = price + (price * 0.15)
+    if "£" in price:
+        price = price.replace("£", "").strip()
+        price = float(price)
+        price = price + (price * 0.15)
+    elif "p" in price:
+        price = price.replace("p", "").strip()
+        price = float(float(price)/100)
+        price = price + (price * 0.15)
     return "£" + str(price)
 
 
 def get_categories_tags(page_obj):
-    tags_concat = []
+    tags_list = []
     tags_ul = page_obj.find("ol", attrs={"class": "ln-c-breadcrumbs ln-o-inline-list"}).findAll("li")
     for tag in tags_ul:
         tag_text = tag.find("a").text.replace("\n", "").replace(",", "").strip()
-        tags_concat.append(tag_text)
-    while len(tags_concat) < 4:
-        tags_concat.append("")
+        tags_list.append(tag_text)
+    tags = "|".join(tags_list)
+    while len(tags_list) < 4:
+        tags_list.append("")
 
-    return tags_concat
+    return (tags_list, tags)
 
 
 def get_alphabets_unit(value):
@@ -42,12 +49,20 @@ def convert_weight_to_kg(weight : str):
     try:
         if unit.lower() == "kg":
             return weight
+        elif unit.lower() == "l":
+            return weight
         elif unit.lower() == "g":
             weight = non_decimal.sub('', weight).strip()
             if "." in weight:
-                weight = str(float(weight) / 1000) + "kg"
+                weight = str(float(weight) / 1000) + "Kg"
             else:
-                weight = str(int(weight) / 1000) + "kg"
+                weight = str(int(weight) / 1000) + "Kg"
+        elif unit.lower() == "ml":
+            weight = non_decimal.sub('', weight).strip()
+            if "." in weight:
+                weight = str(float(weight) / 1000) + "L"
+            else:
+                weight = str(int(weight) / 1000) + "L"
     except Exception as e:
         pass
     return weight
@@ -58,7 +73,7 @@ def find_weight_from_title(title : str):
     splitted_title = title.split(" ")
     for sp_t in splitted_title:
         sp_t = sp_t.split("x")[-1]
-        if "g" in sp_t.lower() or "kg" in sp_t.lower():
+        if "g" in sp_t.lower() or "kg" in sp_t.lower() or "ml" in sp_t.lower() or "l" in sp_t.lower():
             weight = convert_weight_to_kg(sp_t)
             break
     return weight
@@ -68,15 +83,14 @@ def scrape_product(link, selenium_webdriver):
     print("Scraping product link...")
     selenium_webdriver.init_driver()
     selenium_webdriver.webdriver.get(link)
+    time.sleep(4)
     selenium_webdriver.accept_cookies()
     try:
-        time.sleep(3)
         page_src = selenium_webdriver.webdriver.page_source
         page_obj = BeautifulSoup(page_src, "lxml")
 
         prod_title = page_obj.find("h1", attrs={"class": "pd__header"}).text.strip()
-        categories_list = get_categories_tags(page_obj)
-        tags = "|".join(categories_list)
+        categories_list, tags = get_categories_tags(page_obj)
         cost_div = page_obj.find("div", attrs={"class": "pd__cost"})
         price = cost_div.find("div", attrs={"data-test-id": "pd-retail-price"}).text.strip()
         price = increase_price_15_percent(price)
@@ -94,7 +108,7 @@ def scrape_product(link, selenium_webdriver):
 
 def click_no_thanks(selenium_driver):
     try:
-        selenium_driver.webdriver.find_element_by_id("").click()
+        selenium_driver.webdriver.find_element_by_id("smg-etr-invitation-no").click()
     except:
         pass
 
@@ -118,11 +132,9 @@ def scrape_products_page(link, selenium_webdriver, list_scraped_products):
                 pass
 
         try:
-            selenium_webdriver.webdriver.find_element_by_css_selector(
-                '#productLister > div:nth-child(2) > ul.pages > li.next > a').click()
+            selenium_webdriver.webdriver.find_element_by_css_selector('#productLister > div:nth-child(2) > ul.pages > li.next > a').click()
             time.sleep(2)
             counter = 0
-            break
         except Exception as e:
             print(e)
             click_no_thanks(selenium_webdriver)
